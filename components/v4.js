566268551f7b7b6ebd6aadd63f87a221e47dc3b4
Your Name <you@example.com>
2022-11-01 00:21:39 +0530

    fix: add ibl v4 note

---
+++ components/v4.js
@@ -0,0 +1,10 @@
+import Container from "./Container"
+
+export default function V4() {
+	return (
+		<Container>
+			<h2 className="text-semibold text-2xl">Hey there!</h2>
+			<p>If you want to play with the upcoming IBL v4 API or just need slightly more up to date resources, check out the <a className="https://spider.infinitybotlist.com/docs">IBL v4 docs</a></p>
+		</Container>
+	)
+}
